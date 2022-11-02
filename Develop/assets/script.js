c5de4f6b3893abe91bb2453bd0c255a81192fd4e
George Karamanis <gkaramanis1@outlook.com>
2022-11-02 09:44:45 -0400

    added JavaScript content for application to function properly

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -2,8 +2,35 @@
 var currentDay = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").text(currentDay);
 
-    // Created on-click to listen for an event and execute function once save button is clicked
-    $(function () {
+function timeTracker() {
+        var currentTime = moment().hour();
+
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id"));
+
+            // This will check if the block time is less than the current time to color code the time blocks
+            if (blockTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            // This will check if the block time is equal to the current time to color code the time blocks
+            else if (blockTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            // If blockTime is not less than or equal to the current time the future class will be added
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+    // On-click to listen for an event and execute function once save button is clicked
+$(function () {
     $(".saveBtn").on("click", function () {
         var textInput = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
@@ -13,16 +40,16 @@ $("#currentDay").text(currentDay);
     })
 
     // This will get the designated text input for the specific item from the local storage
-    $("#nine .description").val(localStorage.getItem("nine"));
-    $("#ten .description").val(localStorage.getItem("ten"));
-    $("#eleven .description").val(localStorage.getItem("eleven"));
-    $("#twelve .description").val(localStorage.getItem("twelve"));
-    $("#one .description").val(localStorage.getItem("one"));
-    $("#two .description").val(localStorage.getItem("two"));
-    $("#three .description").val(localStorage.getItem("three"));
-    $("#four .description").val(localStorage.getItem("four"));
-    $("#five .description").val(localStorage.getItem("five"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
 
-
+    timeTracker();
 })
 
